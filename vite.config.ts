9334b877e2b31c350204e50b571b46120117c396
Bartoldsson <claes.bartoldsson@gmail.com>
2025-10-15 11:06:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: 'https://bartoldsson.github.io/Kalkylator/', // change to '/<repo>/' for GitHub Pages
+  base: '/Kalkylator/', // change to '/<repo>/' for GitHub Pages
 })
